431510b595c85b8fff6de1101a3fe7e443f3bd18
Aakash Mehta <96887522+aakash240402@users.noreply.github.com>
2025-01-10 21:48:15 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.jpg";
-import editor from "../../Assets/Projects/codeEditor.png";
+import editor from "../../Assets/Projects/codeeditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.jpg";
 import bitsOfCode from "../../Assets/Projects/blog.png";
